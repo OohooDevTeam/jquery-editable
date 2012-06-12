942c4563336c41c29993e692ae3225aa59dd17c0
User <User@10.0.2.15>
2012-06-12 15:38:38 -0600

    Added focus and blur events. Added minWidth property Made minor display improvments.

--- js/jquery_editable.js
+++ js/jquery_editable.js
@@ -4,7 +4,12 @@
         
         var settings = $.extend({
             'nowrap' : true,
-            'maxWidth' : '100%'
+            'maxWidth' : '100%',
+            'minWidth' : '10px',
+            blur : function (el) {
+            },
+            focus: function (el) {
+            }
         }, options);
         
         
@@ -13,12 +18,12 @@
             var editable = $(value);
 
 
-//            //Convert maxWidth to a px value if it is a %
-//            if(settings.maxWidth.indexOf('%') >= 0) {
-//                settings.maxWidth = settings.maxWidth.substring(0, settings.maxWidth.length - 1); // Remove las char.
-//                settings.maxWidth = (settings.maxWidth / 100) * editable.parent().width(); //Get size in pixels
-//                settings.maxWidth += 'px';
-//            }
+            //            //Convert maxWidth to a px value if it is a %
+            //            if(settings.maxWidth.indexOf('%') >= 0) {
+            //                settings.maxWidth = settings.maxWidth.substring(0, settings.maxWidth.length - 1); // Remove las char.
+            //                settings.maxWidth = (settings.maxWidth / 100) * editable.parent().width(); //Get size in pixels
+            //                settings.maxWidth += 'px';
+            //            }
             
             //Make sure you can select all elements.
             editable.css( {
@@ -31,6 +36,7 @@
                 
                 'display': 'inline-block',
                 'max-width' : settings.maxWidth,
+                'min-width' : settings.minWidth,
                 'overflow' : 'hidden'
             });
             
@@ -52,9 +58,9 @@
                 'border-style' : editable.css('border-style'),
                 'border-color' : editable.css('border-color'),
                 'border-width' : editable.css('border-width'),
-//                'position' : editable.css('position'),
-//                'top' : editable.position().top,
-//                'left': editable.position().left
+            //                'position' : editable.css('position'),
+            //                'top' : editable.position().top,
+            //                'left': editable.position().left
 
             };
             
@@ -63,31 +69,46 @@
                     editable.css({
                         'outline': 'none',
                         'overflow' :'hidden',
-                        'background-color' : 'white',
                         'border-style' : 'solid',
                         'border-color' : '#efc868',
-                        'border-width' : '1px',
-//                        'position': 'absolute',
-//                        'top' : editable.position().top - 1, //Border
-//                        'left': editable.position().left - 1 //border
+                        'border-width' : '1px'
+                    //                        'position': 'absolute',
+                    //                        'top' : editable.position().top - 1, //Border
+                    //                        'left': editable.position().left - 1 //border
                     });
             
                     editable.attr('contenteditable', 'true'); 
                     editable.focus();
+                    settings.focus(editable);
                 }
+                
+                editable.addClass('ui-state-hover');
                 
             });
 
             //If you click outside the text element treat it as a submit.
             editable.blur(function() {
+                //For some reason it adds <br> to end of it if it becomes empty at some point =/
+                editable.html(editable.html().replace('<br>', ''));
+                
+                if(editable.html().length == 0) {
+                    editable.html('&lt;empty&gt;');
+                }
                 editable.css(old_css);
                 editable.attr('contenteditable', 'false');
+                editable.removeClass('ui-state-hover');
+                settings.blur(editable);
+                
+
             });
             
             editable.on("keydown", function(event) {
                 if (event.keyCode == 13) {
                     event.preventDefault();
+                    event.stopPropagation();
                     editable.blur();
+                    return false;
+
                 }
             });
             
